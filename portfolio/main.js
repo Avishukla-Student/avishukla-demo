5f9b742871a393b51dc171d0f57fcdbbbaa35942
Avishukla-student <avishukla8303@gmail.com>
2024-12-13 10:03:53 +0530

    add a file

--- portfolio/main.js
+++
@@ -1,9 +0,0 @@
-var options = {
-    Strings:['frontend developer','web developer',"Collage Student"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-};
-
-var typed= new typed(".text", options);
